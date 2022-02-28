f8509273e079a745695f8ac94d48a70db7e6162a
Adriano Geraci <adriano.geraci00@gmail.com>
2022-02-28 17:06:46 -0300

    visto

--- src/app/components/pages/notificaciones/notificaciones-list/content/content.component.ts
+++ src/app/components/pages/notificaciones/notificaciones-list/content/content.component.ts
@@ -47,7 +47,11 @@ export class ContentComponent implements OnInit {
         hide: true,
       },
 
-
+      id: {
+        title: 'Tipo Aviso',
+        filter: true,
+        hide: true,
+      },
 
     },
     noDataMessage: 'No hay nuevas notificaciones'
